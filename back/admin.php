ee6dcdcdc7950c674242478d1fe9e8e3da3cee2d
miakojason <miakojason@gmail.com>
2024-02-28 23:40:00 +0800

    function寫出來很順開始覺得自己有向工程師菜鳥等級了哈哈哈

--- back/admin.php
+++ back/admin.php
@@ -56,5 +56,32 @@
     </tr>
 </table>
 <script>
-    
+    function clean(){
+        $("input[type='text'],input[type='password'],input[type='radio']").val("")
+    }
+    function login(){
+        let user={
+            acc:$("#acc").val(),
+            pw:$("#pw").val(),
+            pw2:$("#pw2").val(),
+            email:$("#email").val()
+        }
+        if(user.acc!='' && user.acc!='' && user.acc!='' && user.acc!=''){
+            if(user.pw==user.pw2){
+                $.post("./api/chk_acc.php",{acc:user.acc},(res)=>{
+                    if(parseInt(res)==1){
+                        alert("帳號重複")
+                    }else{
+                        $.post("./api/res.php",user,(res)=>{
+                            location.reload()
+                        })
+                    }
+                })
+            }else{
+                alert("密碼錯誤")
+            }
+        }else{
+            alert("不可空白")
+        }
+    }
 </script>
